95f66fde654e8b4d1392d972346a0e2a58471fd6
X-tymchenko <xtymchenko@ex.ua>
2017-09-30 20:14:13 +0300

    add auth

--- main.js
+++ main.js
@@ -1,37 +1,31 @@
 const electron = require('electron')
-// Module to control application life.
 const app = electron.app
-// Module to create native browser window.
-const BrowserWindow = electron.BrowserWindow
+const BrowserWindow = electron.BrowserWindow;
 
 const path = require('path')
 const url = require('url')
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
-
+let windowOptions = {
+  width: 1080,
+  minWidth: 680,
+  height: 840,
+  // frame: false,
+  title: 'Stack Overflow'
+};
 function createWindow () {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow(windowOptions)
 
-  // and load the index.html of the app.
   mainWindow.loadURL(url.format({
     pathname: path.join(__dirname, 'index.html'),
     protocol: 'file:',
     slashes: true
   }))
+  mainWindow.webContents.openDevTools()
 
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
-
-  // Emitted when the window is closed.
   mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
     mainWindow = null
-  })
+  });
 }
 
 app.on('ready', createWindow)
--- renderer.js
+++ renderer.js
@@ -1,3 +1,88 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// All of the Node.js APIs are available in this process.
+window.$ = window.jQuery = require('jquery');
+global.Tether = require('tether');
+global.Bootstrap = require('bootstrap');
+const stackexchange = require("./src/javascript/stackexchange-auth");
+
+$(()=>{
+ $("#login-button").on('click', (e)=>{
+  e.preventDefault();
+  console.log(stackexchange);
+  stackexchange.auth();
+ });
+});
+
+// console.log(auth({ token: token, expires: expires }));
+
+// const {BrowserWindow} = require('electron').remote
+// const win = new BrowserWindow({
+//   width: 800,
+//   height: 600,
+//   alwaysOnTop: true,
+//   webPreferences: {
+//     nodeIntegration: false
+//   }
+// });
+
+// win.loadURL('https://stackexchange.com/oauth/dialog?redirect_uri=https://stackexchange.com/oauth/login_success&client_id=10932&scope=write_access private_info read_inbox')
+
+// authWindow.webContents.on('new-window', (event, url, frameName, disposition, options, additionalFeatures) => {
+//   if (frameName === 'modal') {
+//     // open window as modal
+//     event.preventDefault()
+//     Object.assign(options, {
+//       modal: true,
+//       parent: mainWindow,
+//       width: 100,
+//       height: 100
+//     })
+//     event.newGuest = new BrowserWindow(options)
+//   }
+// })
+
+// const loadAuthUrl = () => {
+//   authWindow.loadURL('https://stackexchange.com/oauth/dialog?redirect_uri=https://stackexchange.com/oauth/login_success&client_id=10932&scope=write_access private_info read_inbox');
+// };
+
+// loadAuthUrl();
+
+// // Extract `did-finish-load` handler to own function so we can unbind it later
+// const showAuthWindowIfNotLoggedIn = () => {
+//   authWindow.show();
+// };
+
+// const unloadAndCloseAuthWindow = () => {
+//   authWindow.webContents.removeListener('did-finish-load', showAuthWindowIfNotLoggedIn);
+//   authWindow.destroy();
+// };
+
+// authWindow.webContents.on('did-get-redirect-request', (event, oldUrl, newUrl) => {
+//   var isMainPage = !/[a-zA-Z]+/.test(newUrl.replace(/(https|http)/, '').replace('//stackexchange.com', ''));
+
+//   if (isMainPage) {
+//     return loadAuthUrl();
+//   }
+
+//   const isError = newUrl.indexOf('error') >= 0;
+//   const hasToken = newUrl.indexOf('access_token') >= 0;
+
+//   if (isError || !hasToken) {
+//     return;
+//   }
+
+//   // Success authentication
+//   const hashPosition = newUrl.indexOf('#') + 1;
+//   let [token, expires] = newUrl.substring(hashPosition).split('&');
+//   token = token.split('=')[1];
+//   expires = expires.split('=')[1];
+  
+//   scb(token, expires);
+
+//   unloadAndCloseAuthWindow();
+// });
+
+// // This event can be unregistered by `did-get-redirect-request` handler
+// authWindow.webContents.on('did-finish-load', showAuthWindowIfNotLoggedIn);
+
+// authWindow.on('closed', () => {
+//   authWindow = null;
+// });
