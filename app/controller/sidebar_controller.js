c45cfa0cd04717c6804590d7180b17f77d013a6d
X-tymchenko <xtymchenko@ex.ua>
2017-10-19 12:10:52 +0300

    add render method

---
+++ app/controller/sidebar_controller.js
@@ -0,0 +1,29 @@
+import servicesApi from '../services/stackexchange-service';
+import db from '../data/dataStore';
+
+const SideBar = class Sidebarcontroller{
+  constructor(){
+     this.profile;
+  }
+  async getMe(){
+    let profile;
+    await servicesApi.fetch('/me', { access_token: db.get("access_token") }).then(response => {
+      profile = response.items[0];
+      db.set({profile: profile});
+    });
+    return profile;
+  };
+  async render(){
+    const profile = await this.getMe();
+    console.log(profile);
+    return document.querySelector("#sidebar").insertAdjacentHTML('beforeend',`<div class = "sidebar">
+      <figure>
+        <img src="${profile.profile_image}" alt="" sizes="" srcset="">
+      </figure>
+    </div>`);
+  }
+}
+
+
+
+export default SideBar;
--- renderer.js
+++ renderer.js
@@ -5,22 +5,17 @@ global.Bootstrap = require('bootstrap');
 import { remote } from 'electron';
 import db from './app/data/dataStore';
 import Auth from './app/services/stackExchangeAuth';
+import SideBar from './app/controller/sidebar_controller.js'
 import servicesApi from './src/javascript/stackexchange-service';
 import questionServices from './src/javascript/question-service';
 
 $(() => {
   Auth.login();
-  servicesApi.fetch('/me', { access_token: db.get("access_token") }).then(response => {
-    const name = response.items[0].display_name;
-    document.querySelector(".container").insertAdjacentHTML('beforeend', '<button type="button" id="logOut" class="btn btn-lg">log uut</button>');
-    document.querySelector('#logOut').addEventListener('click', () => {
-      servicesApi.logOut({ access_token: localStorage.token });
-    });
-    questionServices.getQuestions('/questions', {
-      pagesize: 20,
-      order: 'desc',
-      sort: 'activity',
-      tagged: "ruby-on-rails"
-    });
-  });
+  const bar = new SideBar().render();
+  // questionServices.getQuestions('/questions', {
+  //   pagesize: 20,
+  //   order: 'desc',
+  //   sort: 'activity',
+  //   tagged: "ruby-on-rails"
+  // });
 });
