d5960d6ed3d68a2978b6ed798d60973d28e7233d
hrushch <andrijmessia@gmai.com>
2017-11-11 21:33:54 +0200

    Add Observer pattern

--- renderer.js
+++ renderer.js
@@ -10,15 +10,33 @@ const ipc = electron.ipcRenderer;
 const user = require('./app/controller/UserController');
 const RequestServices = require('./app/services/RequestServices').request();
 const UserProfile = require('./app/controller/UserProfile')._();
+const Observer = require('./app/observer/pubsub').observer();
+
 const win = remote.getCurrentWindow();
-let profile;
-const container = document.querySelector('.container');
+let statusLoad = false;
+document.addEventListener('click', e => e.preventDefault());
+Observer.subscribe('initLinks', status, data => {
+    if ((statusLoad = data) === true) {
+        document.querySelector('[data-link="questions"]').addEventListener('click', (e) => {
+            console.log('questions');
+        });
+        document.querySelector('[data-link="users"]').addEventListener('click', (e) => {
+            console.log('users');
+        });
+        document.querySelector('[data-link="tags"]').addEventListener('click', (e) => {
+            console.log('tags');
+        });
+    }
+    Observer.unsubscribe('initLinks');
+});
 
 ipc.on('sidebar:initialize', async (event)=>{
     const userProfile = new UserProfile();
     await userProfile.render();
-    renderHeader(userProfile);
+    await renderHeader(userProfile);
+    Observer.publish('initLinks', statusLoad = true)
 });
+
 
 ipc.on('stackexchange:login', (event, data) => {
     localStorage.token = data.token;
@@ -26,28 +44,40 @@ ipc.on('stackexchange:login', (event, data) => {
     win.webContents.send('sidebar:initialize');
 });
 const renderHeader = (userProfile) => {
-    container.innerHTML = `
-    <nav class="navbar navbar-toggleable-md navbar-light bg-faded">
-      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" 
-      aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    document.body.insertAdjacentHTML('afterbegin', `
+    <nav class="navbar navbar-expand-lg navbar-light bg-light p-0">
+      <div class="container">
+      <a class="navbar-brand" href="/">
+        <img src="./app/assets/img/logo.png" height="60" alt="Stack Overflow">
+      </a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-      <a class="navbar-brand" href="#">StackExchange</a>
-      <div class="collapse navbar-collapse" id="navbarNavDropdown">
-        <ul class="navbar-nav">
+    
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
           <li class="nav-item active">
-            <a class="nav-link nav-link-active" rel="nofollow">Home <span class="sr-only">(current)</span></a>
+            <a class="nav-link" href="/">StackExchange <span class="sr-only">(current)</span></a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" rel="nofollow">Add Question</a>
+            <a class="nav-link" data-link='questions' href='/questions' >Questions</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" rel="nofollow">Users</a>
+            <a class="nav-link" data-link='tags' href='/tags' >Tags</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" rel="nofollow">Tags</a>
+            <a class="nav-link" data-link='users' href='/users' >Users</a>
+          </li>
+          <li  class="nav-item">
+            <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            </form>
           </li>
         </ul>
+        
       </div>
-    </nav>`;
+      </div>
+    </nav>`);
 }
+
